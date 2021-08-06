17032db8c444c39e7bfc3f6be49d5f5a37bcb7ed
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-06 10:16:58 +0200

    Brew formula update for tailbox version v0.0.3

--- Formula/tailbox.rb
+++ Formula/tailbox.rb
@@ -5,28 +5,28 @@
 class Tailbox < Formula
   desc ""
   homepage ""
-  version "0.0.2"
+  version "0.0.3"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/ruudk/tailbox/releases/download/v0.0.2/tailbox_0.0.2_darwin_amd64.tar.gz"
-      sha256 "13946da27611dc844b1e89ace82c172d3cf07e619f6d8b1997ea36d6fc332aba"
+      url "https://github.com/ruudk/tailbox/releases/download/v0.0.3/tailbox_0.0.3_darwin_amd64.tar.gz"
+      sha256 "c7fbd6996a7894fc97776e73942e2fa44e15f4e40e944e6bcf3560c230527c28"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/ruudk/tailbox/releases/download/v0.0.2/tailbox_0.0.2_darwin_arm64.tar.gz"
-      sha256 "7d660c45fcd4cd882ec0795107c3c578eef733b39f543526090d7bf088e406de"
+      url "https://github.com/ruudk/tailbox/releases/download/v0.0.3/tailbox_0.0.3_darwin_arm64.tar.gz"
+      sha256 "3532f739fcc72deaf7d99b6f3bbde7ec0d8cdedeaf9cddcfc3ffd0e5f869dc11"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/ruudk/tailbox/releases/download/v0.0.2/tailbox_0.0.2_linux_amd64.tar.gz"
-      sha256 "fcf3ab2f2d655f499029da8c2ef8cf22bd29482d5c990ce311834c936aa19da6"
+      url "https://github.com/ruudk/tailbox/releases/download/v0.0.3/tailbox_0.0.3_linux_amd64.tar.gz"
+      sha256 "c90198eee2a92dc54aeee923e2c05cb0e97bf76d623c6e8671edc4713d968afc"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/ruudk/tailbox/releases/download/v0.0.2/tailbox_0.0.2_linux_arm64.tar.gz"
-      sha256 "724d9ad6c9d573da47cbd37fbcee8da85c8298655b3edbda16e5c87f2088eb1e"
+      url "https://github.com/ruudk/tailbox/releases/download/v0.0.3/tailbox_0.0.3_linux_arm64.tar.gz"
+      sha256 "27e4f614a9bd038b0c5dbe50c977d78a8ae74fce634ba3ee847c364a765cc8bb"
     end
   end
 
