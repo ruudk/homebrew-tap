de5b7985ad7f74c8da3d394a708f7237863795e1
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-03 13:36:39 +0200

    Brew formula update for tailbox version v0.0.11

--- Formula/tailbox.rb
+++ Formula/tailbox.rb
@@ -5,28 +5,28 @@
 class Tailbox < Formula
   desc ""
   homepage ""
-  version "0.0.10"
+  version "0.0.11"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/ruudk/tailbox/releases/download/v0.0.10/tailbox_0.0.10_darwin_amd64.tar.gz"
-      sha256 "0bc7d956348a12dc7ad7974600c3546105c95f80ddbb4b649efe4009f5c9aeae"
+      url "https://github.com/ruudk/tailbox/releases/download/v0.0.11/tailbox_0.0.11_darwin_amd64.tar.gz"
+      sha256 "2f1a7edf114243918c7b91d03200d283f0e10b959b356aa0ee9ad574a36bf1cb"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/ruudk/tailbox/releases/download/v0.0.10/tailbox_0.0.10_darwin_arm64.tar.gz"
-      sha256 "3ee45b33a5fcc43c28b6fb74e98f5fa8f41ade747cd6c14012cc736480112944"
+      url "https://github.com/ruudk/tailbox/releases/download/v0.0.11/tailbox_0.0.11_darwin_arm64.tar.gz"
+      sha256 "7ad13309d33d011c8d19eb3b6e418b7b6d5c58cf88069f981f663678e3d98517"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/ruudk/tailbox/releases/download/v0.0.10/tailbox_0.0.10_linux_amd64.tar.gz"
-      sha256 "956e968fcb4cf3838019e5b1be3781b4fad990574dea0a8e9eedb938f48a8444"
+      url "https://github.com/ruudk/tailbox/releases/download/v0.0.11/tailbox_0.0.11_linux_amd64.tar.gz"
+      sha256 "a06eedc064b29c181de0c708f5a1cb468187819bf644d06149ad73e86c6ae11a"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/ruudk/tailbox/releases/download/v0.0.10/tailbox_0.0.10_linux_arm64.tar.gz"
-      sha256 "c282304bfb3a1820e3c5da04893a754c777edb9fb7382b4867883f68a7abc61c"
+      url "https://github.com/ruudk/tailbox/releases/download/v0.0.11/tailbox_0.0.11_linux_arm64.tar.gz"
+      sha256 "e0764ee35903336aa9e14689e3384a4ef61d5c3571164f2399a14cb362757d79"
     end
   end
 
